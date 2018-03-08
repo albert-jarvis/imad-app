edb2581b2b2eea58ea36f3cf8103ea54b9187937
albert-jarvis <albert.jarvis@outlook.com>
2018-03-08 15:52:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,78 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one':{
+        title:'Article 1 | Albert Abraham',
+        heading:'Article 1',
+        date:'8th March',
+        content:`
+            <p>
+                Est dolore sed consequatur autem ut tenetur est ut. Corporis eos qui eveniet aut et consectetur. Itaque provident autem
+                impeditiure consectetur. Vel quod ipsam ut ut. Id quos impedit consequatur nemo sequi facilis non voluptates. Est aut ut qui.
+            </p>
+            <p>
+                Est dolore sed consequatur autem ut tenetur est ut. Corporis eos qui eveniet aut et consectetur. Itaque provident autem
+                impeditiure consectetur. Vel quod ipsam ut ut. Id quos impedit consequatur nemo sequi facilis non voluptates. Est aut ut qui.
+            </p>
+            <p>
+                Est dolore sed consequatur autem ut tenetur est ut. Corporis eos qui eveniet aut et consectetur. Itaque provident autem
+                impeditiure consectetur. Vel quod ipsam ut ut. Id quos impedit consequatur nemo sequi facilis non voluptates. Est aut ut qui.
+            </p>`
+    },
+    'article-two':{
+        title:'Article 1 | Albert Abraham',
+        heading:'Article 1',
+        date:'8th March',
+        content:`
+            <p>
+                Est dolore sed consequatur autem ut tenetur est ut. Corporis eos qui eveniet aut et consectetur. Itaque provident autem
+                impeditiure consectetur. Vel quod ipsam ut ut. Id quos impedit consequatur nemo sequi facilis non voluptates. Est aut ut qui.
+            </p>
+            <p>
+                Est dolore sed consequatur autem ut tenetur est ut. Corporis eos qui eveniet aut et consectetur. Itaque provident autem
+                impeditiure consectetur. Vel quod ipsam ut ut. Id quos impedit consequatur nemo sequi facilis non voluptates. Est aut ut qui.
+            </p>
+            <p>
+                Est dolore sed consequatur autem ut tenetur est ut. Corporis eos qui eveniet aut et consectetur. Itaque provident autem
+                impeditiure consectetur. Vel quod ipsam ut ut. Id quos impedit consequatur nemo sequi facilis non voluptates. Est aut ut qui.
+            </p>`
+    }
+};
+
+
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var header = data.header;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>${title}</title>
+            
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="ui/style.css" rel="stylesheet"/>
+
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">HOME</a>
+                </div>
+                <h3>${header}</h3>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -16,6 +88,13 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
