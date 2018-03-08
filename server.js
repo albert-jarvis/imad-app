545ac8e9f2d2bc2a33fad91021a362138769823c
albert-jarvis <albert.jarvis@outlook.com>
2018-03-08 16:09:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -93,10 +98,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
+
 
 
 
